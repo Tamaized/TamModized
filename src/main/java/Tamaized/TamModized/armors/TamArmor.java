74fc8a7db86406aa65b3fd138dd042653d5d0e43
Tamaized <ryanolt123@gmail.com>
2016-07-06 00:04:06 -0700

    armor

---
+++ src/main/java/Tamaized/TamModized/armors/TamArmor.java
@@ -0,0 +1,29 @@
+package Tamaized.TamModized.armors;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import Tamaized.TamModized.registry.ITamModel;
+
+public class TamArmor extends ItemArmor implements ITamModel{
+	
+	private final String name;
+	
+	public TamArmor(String modid, CreativeTabs tab, ArmorMaterial armorMaterial, int par3, EntityEquipmentSlot par4, String type, String n) {
+		super(armorMaterial, par3, par4);
+		name = n;
+		setUnlocalizedName(name);
+		GameRegistry.register(this, new ResourceLocation(modid, "armors/"+n));
+		this.setMaxStackSize(1);
+		this.setCreativeTab(tab);
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+
+}
