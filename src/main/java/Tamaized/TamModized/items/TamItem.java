bc020dcfc690cc6ddd6b9dd7150514286b266aee
Tamaized <ryanolt123@gmail.com>
2016-07-06 00:01:01 -0700

    item

---
+++ src/main/java/Tamaized/TamModized/items/TamItem.java
@@ -0,0 +1,24 @@
+package Tamaized.TamModized.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import Tamaized.TamModized.registry.ITamModel;
+
+public class TamItem extends Item implements ITamModel {
+
+	private final String name;
+
+	public TamItem(CreativeTabs tab, String n) {
+		super();
+		name = n;
+		setUnlocalizedName(name);
+		GameRegistry.register(this.setRegistryName("items/" + n));
+		this.setCreativeTab(tab);
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+}
