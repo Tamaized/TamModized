3ed5a337caca4cb024fdf8eef1228841dce43792
Tamaized <ryanolt123@gmail.com>
2016-07-06 10:44:31 -0700

    block stuff

--- src/main/java/Tamaized/TamModized/blocks/TamBlock.java
+++ src/main/java/Tamaized/TamModized/blocks/TamBlock.java
@@ -3,24 +3,36 @@ package Tamaized.TamModized.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import Tamaized.TamModized.registry.ITamModel;
 
-public abstract class TamBlock extends Block implements ITamModel {
+public class TamBlock extends Block implements ITamModel {
 
 	private final String name;
 
-	public TamBlock(CreativeTabs tab, Material material, String n) {
+	public TamBlock(CreativeTabs tab, Material material, String n, float hardness) {
 		super(material);
 		name = n;
 		setUnlocalizedName(name);
-		GameRegistry.register(this.setRegistryName("blocks/" + n));
+		setHardness(hardness);
+		GameRegistry.register(this.setRegistryName(getModelDir() + "/" + n));
 		this.setCreativeTab(tab);
 	}
 
 	@Override
 	public String getName() {
 		return name;
+	}
+
+	@Override
+	public String getModelDir() {
+		return "blocks";
+	}
+
+	@Override
+	public Item getAsItem() {
+		return Item.getItemFromBlock(this);
 	}
 
 }
--- src/main/java/Tamaized/TamModized/blocks/TamBlockContainer.java
+++ src/main/java/Tamaized/TamModized/blocks/TamBlockContainer.java
@@ -4,6 +4,7 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import Tamaized.TamModized.registry.ITamModel;
@@ -12,11 +13,12 @@ public abstract class TamBlockContainer extends BlockContainer implements ITamMo
 
 	private final String name;
 
-	protected TamBlockContainer(CreativeTabs tab, Material material, String n) {
+	protected TamBlockContainer(CreativeTabs tab, Material material, String n, float hardness) {
 		super(material);
 		name = n;
 		setUnlocalizedName(name);
-		GameRegistry.register(this.setRegistryName("blocks/" + n));
+		setHardness(hardness);
+		GameRegistry.register(this.setRegistryName(getModelDir() + "/" + n));
 		this.setCreativeTab(tab);
 	}
 
@@ -28,6 +30,16 @@ public abstract class TamBlockContainer extends BlockContainer implements ITamMo
 	@Override
 	public EnumBlockRenderType getRenderType(IBlockState state) {
 		return EnumBlockRenderType.MODEL;
+	}
+
+	@Override
+	public String getModelDir() {
+		return "blocks";
+	}
+
+	@Override
+	public Item getAsItem() {
+		return Item.getItemFromBlock(this);
 	}
 
 }
