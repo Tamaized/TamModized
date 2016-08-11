2398e01ebb454748880d6eb565d657d2ad4c5576
Tamaized <ryanolt123@gmail.com>
2016-08-11 11:24:04 -0700

    prevent server side crash

--- src/main/java/Tamaized/TamModized/particles/ParticlePacketBase.java
+++ src/main/java/Tamaized/TamModized/particles/ParticlePacketBase.java
@@ -7,12 +7,15 @@ import java.io.IOException;
 
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.util.math.Vec3d;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import Tamaized.TamModized.particles.ParticleHelper.IParticlePacketData;
 
 public abstract class ParticlePacketBase {
 
 	public abstract void encode(DataOutputStream packet, IParticlePacketData data) throws IOException;
 
+	@SideOnly(Side.CLIENT)
 	public abstract TamParticle decode(ByteBufInputStream packet, WorldClient world, Vec3d pos);
 
 }
