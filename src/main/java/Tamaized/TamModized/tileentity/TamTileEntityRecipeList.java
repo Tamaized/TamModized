e57893e252228a923b5ecd8af2745c298af354eb
Tamaized <ryanolt123@gmail.com>
2016-10-10 00:24:18 -0700

    changed TamTileEntityRecipeList again

--- src/main/java/Tamaized/TamModized/tileentity/TamTileEntityRecipeList.java
+++ src/main/java/Tamaized/TamModized/tileentity/TamTileEntityRecipeList.java
@@ -2,46 +2,48 @@ package Tamaized.TamModized.tileentity;
 
 import java.util.ArrayList;
 
-import Tamaized.TamModized.TamModized;
 import Tamaized.TamModized.tileentity.TamTileEntityRecipeList.TamTERecipe;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public abstract class TamTileEntityRecipeList<T extends TamTERecipe> {
 
-	private ArrayList<T> recipes = new ArrayList<T>();
+	protected ArrayList<T> recipes = new ArrayList<T>();
 
 	public boolean registerRecipe(T recipe) {
 		if (recipes.contains(recipe)) return false;
 		recipes.add(recipe);
-		TamModized.logger.info("(" + getModID() + ":" + getName() + "): " + recipe.getInput() + " => " + recipe.getOutput());
 		return true;
 	}
 
 	public boolean isInput(Item item) {
 		for (T r : recipes) {
-			if (r.getInput().getItem() == item) return true;
+			for (ItemStack stack : r.getInput()) {
+				if (stack.getItem() == item) return true;
+			}
 		}
 		return false;
 	}
 
 	public boolean isInput(ItemStack stack) {
 		for (T r : recipes) {
-			if (r.getInput().areItemStacksEqual(r.getInput(), stack)) return true;
+			for (ItemStack checkStack : r.getInput()) {
+				if (checkStack.areItemStacksEqual(checkStack, stack)) return true;
+			}
 		}
 		return false;
 	}
 
 	public ItemStack getOutput(Item item) {
 		for (T r : recipes) {
-			if (r.getInput().getItem() == item) return r.getOutput();
+			if (isInput(item)) return r.getOutput();
 		}
 		return null;
 	}
 
 	public ItemStack getOutput(ItemStack stack) {
 		for (T r : recipes) {
-			if (r.getInput().areItemStacksEqual(r.getInput(), stack)) return r.getOutput();
+			if (isInput(stack)) return r.getOutput();
 		}
 		return null;
 	}
@@ -55,25 +57,20 @@ public abstract class TamTileEntityRecipeList<T extends TamTERecipe> {
 	 */
 	public class TamTERecipe {
 
-		private final ItemStack input;
+		private final ItemStack[] input;
 		private final ItemStack output;
 
-		public TamTERecipe(ItemStack input, ItemStack output) {
+		public TamTERecipe(ItemStack[] input, ItemStack output) {
 			this.input = input;
 			this.output = output;
 		}
 
-		public ItemStack getInput() {
+		public ItemStack[] getInput() {
 			return input;
 		}
 
 		public ItemStack getOutput() {
 			return output;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			return (obj instanceof TamTileEntityRecipeList.TamTERecipe) ? (input.areItemStacksEqual(input, ((TamTERecipe) obj).getInput()) && (output.areItemStacksEqual(output, ((TamTERecipe) obj).getOutput()))) : false;
 		}
 
 	}
