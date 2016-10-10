b61846897d79c30f3c86cc688bd6d091616c0d2c
Tamaized <ryanolt123@gmail.com>
2016-10-09 22:45:01 -0700

    rewrote TileEntity recipe handling

--- src/main/java/Tamaized/TamModized/tileentity/TamTileEntityRecipeList.java
+++ src/main/java/Tamaized/TamModized/tileentity/TamTileEntityRecipeList.java
@@ -1,87 +1,81 @@
 package Tamaized.TamModized.tileentity;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
 import Tamaized.TamModized.TamModized;
+import Tamaized.TamModized.tileentity.TamTileEntityRecipeList.TamTERecipe;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.oredict.OreDictionary;
 
-public abstract class TamTileEntityRecipeList {
-	
-	/**
-	 * Input, Output
-	 */
-	private Map<Item, TamTERecipe> recipes = new HashMap<Item, TamTERecipe>();
-	private Map<ItemStack, ItemStack> rawMap = new HashMap<ItemStack, ItemStack>();
-	
-	public boolean registerRecipe(ItemStack stack, TamTERecipe recipe){
-		if(isInput(stack.getItem())) return false;
-		recipes.put(stack.getItem(), recipe);
-		rawMap.put(stack, recipe.getResult());
-		TamModized.logger.info("("+getModID()+":"+getName()+"): "+stack.getItem().getUnlocalizedName()+" => "+recipe.result);
+public abstract class TamTileEntityRecipeList<T extends TamTERecipe> {
+
+	private ArrayList<T> recipes = new ArrayList<T>();
+
+	public boolean registerRecipe(T recipe) {
+		if (recipes.contains(recipe)) return false;
+		recipes.add(recipe);
+		TamModized.logger.info("(" + getModID() + ":" + getName() + "): " + recipe.getInput() + " => " + recipe.getOutput());
 		return true;
 	}
-	
-	public boolean registerRecipe(String item, TamTERecipe recipe){
-		for (ItemStack ore : OreDictionary.getOres(item)) {
-			if (ore != null) {
-				return registerRecipe(ore, recipe);
-			}
+
+	public boolean isInput(Item item) {
+		for (T r : recipes) {
+			if (r.getInput().getItem() == item) return true;
 		}
 		return false;
 	}
-	
-	public boolean isInput(Item item){
-		return recipes.containsKey(item);
+
+	public boolean isInput(ItemStack stack) {
+		for (T r : recipes) {
+			if (r.getInput().areItemStacksEqual(r.getInput(), stack)) return true;
+		}
+		return false;
 	}
-	
-	public boolean isInput(ItemStack stack){
-		return stack == null ? null : isInput(stack.getItem());
+
+	public ItemStack getOutput(Item item) {
+		for (T r : recipes) {
+			if (r.getInput().getItem() == item) return r.getOutput();
+		}
+		return null;
 	}
-	
-	public TamTERecipe getOutput(Item item){
-		return recipes.get(item);
+
+	public ItemStack getOutput(ItemStack stack) {
+		for (T r : recipes) {
+			if (r.getInput().areItemStacksEqual(r.getInput(), stack)) return r.getOutput();
+		}
+		return null;
 	}
-	
-	public TamTERecipe getOutput(ItemStack stack){
-		return stack == null ? null : getOutput(stack.getItem());
-	}
-	
-	public ItemStack getResultItem(Item item){
-		if(!isInput(item)) return null;
-		return getOutput(item).getResult();
-	}
-	
-	public ItemStack getResultItem(ItemStack stack){
-		if(stack == null) return null;
-		return getResultItem(stack.getItem());
-	}
-	
-	public Map<ItemStack, ItemStack> getRawMap(){
-		return new HashMap<ItemStack, ItemStack>(rawMap);
-	}
-	
+
 	protected abstract String getName();
-	
+
 	protected abstract String getModID();
-	
+
 	/**
 	 * This holds the end result and additional requirement data
 	 */
 	public class TamTERecipe {
-		
-		private final ItemStack result;
-		
-		public TamTERecipe(ItemStack output){
-			result = output;
+
+		private final ItemStack input;
+		private final ItemStack output;
+
+		public TamTERecipe(ItemStack input, ItemStack output) {
+			this.input = input;
+			this.output = output;
 		}
-		
-		public ItemStack getResult(){
-			return result;
+
+		public ItemStack getInput() {
+			return input;
 		}
-		
+
+		public ItemStack getOutput() {
+			return output;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			return (obj instanceof TamTileEntityRecipeList.TamTERecipe) ? (input.areItemStacksEqual(input, ((TamTERecipe) obj).getInput()) && (output.areItemStacksEqual(output, ((TamTERecipe) obj).getOutput()))) : false;
+		}
+
 	}
 
 }
