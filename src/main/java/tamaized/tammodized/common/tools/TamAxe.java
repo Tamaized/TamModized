860a973533ce7017cab5be7d6d94491e4d65daf0
Tamaized <ryanolt123@gmail.com>
2018-05-24 15:48:35 -0700

    bump forge, mappings, and fix errors

--- src/main/java/tamaized/tammodized/common/tools/TamAxe.java
+++ src/main/java/tamaized/tammodized/common/tools/TamAxe.java
@@ -16,7 +16,7 @@ public class TamAxe extends ItemAxe implements ITamRegistry {
 	private final String name;
 
 	public TamAxe(CreativeTabs tab, ToolMaterial material, String n) {
-		super(material, material.getAttackDamage(), -3.0f);
+		super(material, material.getDamageVsEntity(), -3.0f);
 		name = n;
 		setUnlocalizedName(name);
 		setRegistryName(name);
