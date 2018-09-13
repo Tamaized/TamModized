3570521d4b4a074f360273992ff8a385458d6ec3
Tamaized <ryanolt123@gmail.com>
2018-09-13 04:58:47 -0700

    dnaskjdnwha9wye just null things

--- src/main/java/tamaized/tammodized/common/helper/CapabilityHelper.java
+++ src/main/java/tamaized/tammodized/common/helper/CapabilityHelper.java
@@ -4,13 +4,15 @@ import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 
+import javax.annotation.Nullable;
+
 public class CapabilityHelper {
 
 	private CapabilityHelper() {
 
 	}
 
-	public static <T> T getCap(ICapabilityProvider provider, Capability<T> cap, EnumFacing face) {
+	public static <T> T getCap(@Nullable ICapabilityProvider provider, Capability<T> cap, @Nullable EnumFacing face) {
 		return provider != null && provider.hasCapability(cap, face) ? provider.getCapability(cap, face) : null;
 	}
 
